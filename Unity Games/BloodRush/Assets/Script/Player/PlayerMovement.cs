52ec1edf597062bde791b83558c815673303a933
Edward-AIE <s215765@students.aie.edu.au>
2023-05-18 19:27:39 +1000

    Finished basic movement, added double jump and sprint

--- Unity Games/BloodRush/Assets/Script/Player/PlayerMovement.cs
+++ Unity Games/BloodRush/Assets/Script/Player/PlayerMovement.cs
@@ -6,22 +6,62 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     public float moveSpeed = 6f;
-    public float rbDrag = 6f;
     public float jumpForce;
+    public float doubleJumpForce;
+
+    [Header("Sprinting")]
+    [SerializeField] float walkSpeed;
+    [SerializeField] float sprintSpeed;
+    [SerializeField] float acceleration = 10f;
+
+
+    [Header("Drag")]
+    public float groundDrag = 6f;
+    public float airDrag = 2;
+    [SerializeField] float airMultiplier = -1f;
 
     [Header("Keybinds")]
     [SerializeField] KeyCode jumpKey = KeyCode.Space;
+    [SerializeField] KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("Other")]
+    [SerializeField] Transform orientation;
+    [SerializeField] float groundDistance = 0.4f;
+    [SerializeField] LayerMask groundLayer;
 
     float horizontalMovement;
     float verticalMovement;
     float playerHeight = 2f;
     
     bool isGrounded;
+    private bool hasJumped;
 
     Vector3 moveDirection;
+    Vector3 slopeMoveDirection;
 
     Rigidbody rb;
 
+    RaycastHit slopeHit;
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.1f))
+        {
+            if (slopeHit.normal != Vector3.up)
+            {
+               return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -30,15 +70,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight / 2 + 0.1f);
-        
-        MyInput();
-        ControlDrag();
+        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1, 0), groundDistance, groundLayer);
+
+        if (isGrounded)
+        {
+            hasJumped = false;
+        }
 
         if (Input.GetKeyDown(jumpKey) && isGrounded)
         {
             Jump();
         }
+
+        if (Input.GetKeyDown(jumpKey) && !isGrounded && !hasJumped)
+        {
+            DoubleJump();
+            hasJumped = true;
+        }
+
+        slopeMoveDirection = Vector3.ProjectOnPlane(moveDirection, slopeHit.normal);
+
+        MyInput();
+        ControlDrag();
+        ControlSpeed();
     }
 
     private void FixedUpdate()
@@ -51,21 +105,56 @@ public class PlayerMovement : MonoBehaviour
         horizontalMovement = Input.GetAxisRaw("Horizontal");
         verticalMovement = Input.GetAxisRaw("Vertical");
 
-        moveDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        moveDirection = orientation.forward * verticalMovement + orientation.right * horizontalMovement;
     }
 
     void MovePlayer()
     {
-        rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+        if (isGrounded && !OnSlope())
+        {
+            rb.AddForce(moveDirection * moveSpeed, ForceMode.Acceleration);
+
+        }
+        else if(OnSlope() && isGrounded)
+        {
+            rb.AddForce(slopeMoveDirection * moveSpeed, ForceMode.Acceleration);
+        }
+        else if (!isGrounded)
+        {
+            rb.AddForce(moveDirection * moveSpeed * airMultiplier, ForceMode.Acceleration);
+        }
     }
 
     void ControlDrag()
     {
-        rb.drag = rbDrag;
+        if (isGrounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = airDrag;
+        }
+    }
+
+    void ControlSpeed()
+    {
+        if (Input.GetKey(sprintKey) && isGrounded)
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, sprintSpeed, acceleration * Time.deltaTime);
+        }
+        else
+        {
+            moveSpeed = Mathf.Lerp(moveSpeed, walkSpeed, acceleration * Time.deltaTime);
+        }
     }
 
     void Jump()
     {
         rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+    void DoubleJump()
+    {
+        rb.AddForce(transform.up * doubleJumpForce, ForceMode.Impulse);
     }
 }
